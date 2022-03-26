514f90fee1f9184f4b18f0483c8f48d0b1e01073
pablo <pablo774@protonmail.com>
2022-03-26 21:24:08 +0100

    ..

--- news/models.py
+++ news/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 
 
+
 class Website(models.Model):
     title = models.CharField(max_length=32)
     url = models.CharField(max_length=128)
--- website/forms.py
+++ website/forms.py
@@ -2,12 +2,14 @@ from django import forms
 from markets.models import Currency
 
 
+
 class SelectCurrency(forms.Form):
     currency = forms.ChoiceField(label='Currency', choices=(), required=True)
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.fields['currency'].choices = [(c.id, c.name) for c in Currency.objects.all()]
+
 
 
 class SetEmail(forms.Form):
